741385db150577827a042c1f3ffce6fb54aedccb
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-08-19 22:09:20 +0700

    nav

--- lib/peges/homepage.dart
+++ lib/peges/homepage.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: const Center(child: Text('เนื้อหาหลัก')),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            IconButton(icon: Icon(Icons.home), onPressed: null),
+            IconButton(icon: Icon(Icons.class_sharp), onPressed: null),
+            IconButton(icon: Icon(Icons.navigation), onPressed: null),
+            IconButton(icon: Icon(Icons.person), onPressed: null),
+          ],
+        ),
+      ),
+    );
   }
 }
