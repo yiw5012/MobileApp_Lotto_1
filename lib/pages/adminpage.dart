5ea1d9d260d359639b68ad041428d9673d523e52
Thanapat.Sri <yiwyiw72975012@gmail.com>
2025-09-06 19:46:10 +0700

    admin-yew

--- lib/pages/adminpage.dart
+++ lib/pages/adminpage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lotto_1/pages/Member.dart';
+import 'package:lotto_1/pages/cart.dart';
+import 'package:lotto_1/pages/homepage.dart';
+import 'package:lotto_1/pages/sell.dart';
 
 class Adminpage extends StatefulWidget {
   const Adminpage({super.key});
@@ -8,8 +12,42 @@ class Adminpage extends StatefulWidget {
 }
 
 class _AdminpageState extends State<Adminpage> {
+  int _selectedIndex = 0; // เก็บ index ของ nav bar
+  final List<Widget> _pages = [
+    const HomeContent(), // หน้าแรก
+    const Sell(),
+    const Cartpage(),
+    Member(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _pages[_selectedIndex], // แสดงหน้าที่เลือกอยู่
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // ให้แสดง label ครบทุกอัน
+        currentIndex: _selectedIndex, // index ปัจจุบัน
+        selectedItemColor: Colors.redAccent, // สีตอนเลือก
+        unselectedItemColor: Colors.grey, // สีตอนยังไม่เลือก
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "หน้าแรก"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.class_sharp),
+            label: "ผลรางวัล",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: "ลอตเตอรี่ ",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "สมาชิก"),
+        ],
+      ),
+    );
   }
 }
